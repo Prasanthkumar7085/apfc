ecd1c9e67b9aa6a0150e4317fa91d18396685870
code <code@peepulagri.com>
2024-07-10 14:37:05 +0530

    sw/add user devices device styles

--- src/components/Devices/DeviceSection.tsx
+++ src/components/Devices/DeviceSection.tsx
@@ -17,99 +17,98 @@ const DeviceSection = ({ devicesData }: any) => {
           return (
             <div className="eachDeviceBlock" key={index}>
               <div className="headerBlock">
-                
                 <div className="header">
-                    <h4 className="deviceTItle">{item?.device_name || "--"}</h4>
+                  <h4 className="deviceTItle">{item?.device_name || "--"}</h4>
                   <div className="settingsBlock">
                     <div className="deviceStatus">
-                      <Image  alt="" src="/icondot.svg" height={5} width={5} />
+                      <Image alt="" src="/icondot.svg" height={5} width={5} />
                       <p className="statusTxt">Online</p>
                     </div>
-                  <Image className={styles.iconsetting} alt="" src="/iconsetting.svg"  height={20} width={20}/>
+                    <Image alt="" src="/iconsetting.svg" height={25} width={25} />
                   </div>
                 </div>
-                <div className={styles.devicerow}>
-                  <div className={styles.devicecolumn}>
-                    <p className={styles.averageVoltageLn}>Average Voltage LN</p>
-                    <h5 className={styles.v}>{item?.device_parameters?.voltage_measurements?.average_voltage_ln || "--"}</h5>
+                <div className="deviceInfo">
+                  <div className="eachDeviceInfo">
+                    <p className="infoTitle">Average Voltage LN</p>
+                    <h5 className="infoValue">{item?.device_parameters?.voltage_measurements?.average_voltage_ln || "--"}</h5>
                   </div>
-                  <div className={styles.devicecolumn1}>
-                    <p className={styles.averageVoltageLl}>Average Voltage LL</p>
-                    <h5 className={styles.v1}>{item?.device_parameters?.voltage_measurements?.average_voltage_ll || "--"}</h5>
+                  <div className="eachDeviceInfo">
+                    <p className="infoTitle">Average Voltage LL</p>
+                    <h5 className="infoValue">{item?.device_parameters?.voltage_measurements?.average_voltage_ll || "--"}</h5>
                   </div>
-                  <div className={styles.devicecolumn1}>
-                    <p className={styles.averageVoltageLn}>Average Current</p>
-                    <h5 className={styles.v}>{item?.device_parameters?.voltage_measurements?.average_current || "--"}</h5>
-                  </div>
-                </div>
-             </div>
-                <div className={styles.devicerow1}>
-                  <div className={styles.devicecolumn1}>
-                    <p className={styles.averageVoltageLl}>Total kW</p>
-                    <h5 className={styles.v1}>{item?.device_parameters?.power_measurements?.total_kw || "--"}</h5>
-                  </div>
-                  <div className={styles.devicecolumn1}>
-                    <p className={styles.averageVoltageLn}>Total kVA</p>
-                    <h5 className={styles.v}>{item?.device_parameters?.power_measurements?.total_kva || "--"}</h5>
-                  </div>
-                  <div className={styles.devicecolumn1}>
-                    <p className={styles.averageVoltageLl}>Total kVAr</p>
-                    <h5 className={styles.v1}>{item?.device_parameters?.power_measurements?.total_kvar || "--"}</h5>
-                  </div>
-                  <div className={styles.devicecolumn6}>
-                    <p className={styles.online}>Average PF</p>
-                    <h5 className={styles.h53}>{item?.average_pf || "--"}</h5>
-                  </div>
-                  <div className={styles.devicecolumn1}>
-                    <p className={styles.averageVoltageLl}>kWh</p>
-                    <h5 className={styles.v1}>{item?.device_parameters?.power_measurements?.kwh || "--"}</h5>
+                  <div className="eachDeviceInfo">
+                    <p className="infoTitle">Average Current</p>
+                    <h5 className="infoValue">{item?.device_parameters?.voltage_measurements?.average_current || "--"}</h5>
                   </div>
                 </div>
-                <div className={styles.devicetemperature}>
-                  <div className={styles.temperaturecolumn}>
-                    <img
-                      className={styles.icontemperature}
-                      alt=""
-                      src="/icontemperature.svg"
-                    />
-                    {item?.device_parameters?.errors?.under_compensate_error === true ? (
-                      <div className={styles.errormessage}>
-                        <img className={styles.iconinfo} alt="" src="/iconinfo.svg" />
-                        <h6 className={styles.noVoltageError}>Under Compensate Error</h6>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                  <div className={styles.profilecolumn}>
-                    <Button variant="outlined" onClick={() => setDialogOpen(true)}>
-                      Assign User
-                    </Button>
-                    <div className={styles.profilegroup}>
-                      <Stack direction="row" spacing={2}>
-                        <Avatar sx={{ bgcolor: "orange" }}>
-                          {item?.user?.first_name?.[0]}{item?.user?.last_name?.[0] || "--"}
-                        </Avatar>
-                      </Stack>
-                      {/* <img
+              </div>
+              <div className="deviceInfo" style={{padding:"16px 12px"}}>
+                <div className="eachDeviceInfo">
+                  <p className="infoTitle">Total kW</p>
+                  <h5 className="infoValue">{item?.device_parameters?.power_measurements?.total_kw || "--"}</h5>
+                </div>
+                <div className="eachDeviceInfo">
+                  <p className="infoTitle">Total kVA</p>
+                  <h5 className="infoValue">{item?.device_parameters?.power_measurements?.total_kva || "--"}</h5>
+                </div>
+                <div className="eachDeviceInfo">
+                  <p className="infoTitle">Total kVAr</p>
+                  <h5 className="infoValue">{item?.device_parameters?.power_measurements?.total_kvar || "--"}</h5>
+                </div>
+                <div className="avarageInfo">
+                  <p className="infoTitle">Average PF</p>
+                  <h5 className="infoValue">{item?.average_pf || "--"}</h5>
+                </div>
+                <div className="eachDeviceInfo">
+                  <p className="infoTitle">kWh</p>
+                  <h5 className="infoValue">{item?.device_parameters?.power_measurements?.kwh || "--"}</h5>
+                </div>
+              </div>
+              <div className={styles.devicetemperature}>
+                <div className={styles.temperaturecolumn}>
+                  <img
+                    className={styles.icontemperature}
+                    alt=""
+                    src="/icontemperature.svg"
+                  />
+                  {item?.device_parameters?.errors?.under_compensate_error === true ? (
+                    <div className={styles.errormessage}>
+                      <img className={styles.iconinfo} alt="" src="/iconinfo.svg" />
+                      <h6 className={styles.noVoltageError}>Under Compensate Error</h6>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className={styles.profilecolumn}>
+                  <Button variant="outlined" onClick={() => setDialogOpen(true)}>
+                    Assign User
+                  </Button>
+                  <div className={styles.profilegroup}>
+                    <Stack direction="row" spacing={2}>
+                      <Avatar sx={{ bgcolor: "orange" }}>
+                        {item?.user?.first_name?.[0]}{item?.user?.last_name?.[0] || "--"}
+                      </Avatar>
+                    </Stack>
+                    {/* <img
                         className={styles.profileavatarIcon}
                         alt=""
                         src="/profileavatar@2x.png"
                       /> */}
-                      <h4 className={styles.profilename}>{item?.user?.first_name + " " + item?.user?.last_name || "--"}</h4>
-                    </div>
-                    <div
-                      className={styles.devicestatus1}
-                      onClick={() => {
-                        router.push(`/devices/${item?.id}`)
-                      }}
-                    >
-                      <img className={styles.icon} alt="" src="/icon2.svg" />
-                      <h6 className={styles.noVoltageError}>View</h6>
-                    </div>
+                    <h4 className={styles.profilename}>{item?.user?.first_name + " " + item?.user?.last_name || "--"}</h4>
+                  </div>
+                  <div
+                    className={styles.devicestatus1}
+                    onClick={() => {
+                      router.push(`/devices/${item?.id}`)
+                    }}
+                  >
+                    <img className={styles.icon} alt="" src="/icon2.svg" />
+                    <h6 className={styles.noVoltageError}>View</h6>
                   </div>
                 </div>
-              
+              </div>
+
             </div>
           )
         })}
