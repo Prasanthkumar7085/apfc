3c731f2006a4a5d6fd3a971c4793e655958f3c3f
code <code@peepulagri.com>
2024-07-10 12:51:01 +0530

    sw/add styles to view user profile section

--- src/components/ListUsers/SingleUserView.tsx
+++ src/components/ListUsers/SingleUserView.tsx
@@ -41,40 +41,39 @@ const SingleUserView = () => {
                 className="backBtn"
                 sx={{ alignSelf: 'flex-start', mb: 2 }}
                 onClick={() => router.back()}
-                startIcon={<Image src="/users/back-icon.svg" alt="" width={13} height={13} />}
-
-            >
+                startIcon={<Image src="/users/back-icon.svg" alt="" width={13} height={13} />}  >
                 Back
             </Button>
-            <div className="profilegroup">
+            <div className="userInfo">
+                <div className="profileGrp">
                 <Avatar className="profileavatarIcon">
                     {usersData?.first_name?.[0]}{usersData?.last_name?.[0]}
                 </Avatar>
-                <div>
-                    <h4 className="profilename">{usersData?.first_name + " " + usersData?.last_name}</h4>
-                    <div className="iconstatus">
-                        <Image className="statusicon" alt="" src="/Completed 1.svg" width={10} height={10} />
-                        <span>{usersData?.status}</span>
-                    </div>
-                    <div className="userinfo">
-                        <Image alt="" src="/calendar-blank 1.svg" width={15} height={15} />
-                        <span>{usersData?.updated_at}</span>
-                    </div>
-                    <div className="userinfo">
-                        <Image alt="" src="/mail.svg" width={15} height={15} />
-                        <span>{usersData?.email}</span>
-                    </div>
-                    <div className="userinfo">
-                        <Image alt="" src="/phone.svg" width={15} height={15} />
-                        <span>{usersData?.phone}</span>
-                    </div>
+                <h4 className="profileName">{usersData?.first_name + " " + usersData?.last_name}</h4>
+                <div className="status">
+                    <Image alt="" src="/Completed 1.svg" width={12} height={12} />
+                        <span>{usersData?.status?.charAt(0).toUpperCase() + usersData?.status?.slice(1)}    </span>
                 </div>
+
+            </div>
+            <div className="userinfo">
+                <Image alt="" src="/calendar-blank 1.svg" width={15} height={15} />
+                <span>{usersData?.updated_at}</span>
+            </div>
+            <div className="userinfo">
+                <Image alt="" src="/mail.svg" width={15} height={15} />
+                <span>{usersData?.email}</span>
+            </div>
+            <div className="userinfo">
+                <Image alt="" src="/phone.svg" width={15} height={15} />
+                <span>{usersData?.phone}</span>
             </div>
             <div className="devicesection">
                 <h4>Devices</h4>
                 <DeviceSection
                     devicesData={data}
                 />
+            </div>
             </div>
             <LoadingComponent loading={loading} />
         </div>
