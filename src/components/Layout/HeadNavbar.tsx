87d55d15d3386601de0489b61ea24f99099586e3
code <code@peepulagri.com>
2024-07-10 10:33:00 +0530

    sw/add styles to nav filters

--- src/components/Layout/HeadNavbar.tsx
+++ src/components/Layout/HeadNavbar.tsx
@@ -2,6 +2,7 @@ import {
   Button,
   FormControl,
   FormHelperText,
+  InputAdornment,
   InputLabel,
   MenuItem,
   Select,
@@ -15,89 +16,55 @@ const HeadNavbar = () => {
   const path = usePathname();
   return (
     <div className="headnav">
-      <h4 className="pagetitle"> {path.includes("/users") ? "Users" :"Devices"} </h4>
-      <div className="searchsection">
-        <div className="searchgroup">
-          <TextField
-            className="inputtype"
+      <h4 className="pagetitle"> {path.includes("/users") ? "Users" : "Devices"} </h4>
+      <div className="navActions">
+        <TextField
+          className="defaultTextFeild"
+          defaultValue="Search"
+          variant="outlined"
+          type="search"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Image src="/users/search-icon.svg" alt="" width={15} height={15} />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <FormControl
+          className="defaultSelect"
+          variant="outlined"
+        >
+          <Select
             color="primary"
-            defaultValue="Search"
-            variant="outlined"
-            type="search"
-            sx={{ "& .MuiInputBase-root": { height: "39px" }, width: "303px" }}
-          />
-          <FormControl
-            className="selectdevice"
-            variant="outlined"
-            sx={{
-              borderRadius: "0px 0px 0px 0px",
-              width: "220px",
-              height: "39px",
-              m: 0,
-              p: 0,
-              "& .MuiInputBase-root": {
-                m: 0,
-                p: 0,
-                minHeight: "39px",
-                justifyContent: "center",
-                display: "inline-flex",
-              },
-              "& .MuiInputLabel-root": {
-                m: 0,
-                p: 0,
-                minHeight: "39px",
-                display: "inline-flex",
-              },
-              "& .MuiMenuItem-root": {
-                m: 0,
-                p: 0,
-                height: "39px",
-                display: "inline-flex",
-              },
-              "& .MuiSelect-select": {
-                m: 0,
-                p: 0,
-                height: "39px",
-                alignItems: "center",
-                display: "inline-flex",
-              },
-              "& .MuiInput-input": { m: 0, p: 0 },
-              "& .MuiInputBase-input": { textAlign: "left", p: "0 !important" },
-            }}
           >
-            <InputLabel color="primary">Select Device</InputLabel>
-            <Select
-              color="primary"
-              label="Select Device"
-              disableUnderline
-              displayEmpty
-            >
-              <MenuItem value="Device 1">Device 1</MenuItem>
-              <MenuItem value="Device 2">Device 2</MenuItem>
-              <MenuItem value="Device 3">Device 3</MenuItem>
-            </Select>
-            <FormHelperText />
-          </FormControl>
-        </div>
+            <MenuItem value="">Select Device</MenuItem>
+            <MenuItem value="Device 1">Device 1</MenuItem>
+            <MenuItem value="Device 2">Device 2</MenuItem>
+            <MenuItem value="Device 3">Device 3</MenuItem>
+          </Select>
+        </FormControl>
+
         {path?.includes("/users") ? (
-          <div>
-            <Button
-              variant='contained'
-              onClick={() => router.push("/users/add")}
-            >
-              Add User
-            </Button>
-          </div>
+          <Button
+            className="addUserBtn"
+            variant='contained'
+            onClick={() => router.push("/users/add")}
+            startIcon={<Image src="/users/add-icon.svg" alt="" height={10} width={10}/>}
+          >
+            Add New User
+          </Button>
+
         ) : (
           ""
         )}
         <div className="profilegroup">
-          <Image className="avatarIcon" alt="" src="/avatar@2x.png" height={30} width={30} />
+          <Image className="avatarIcon" alt="" src="/avatar@2x.png" height={20} width={20} />
           <div className="profilename">
             <h4 className="profile">Ansh Kalasannavar</h4>
             <p className="designation">Admin</p>
           </div>
-          <Image className="icon" alt="" src="/icon1.svg" height={30} width={30} />
+          <Image className="icon" alt="" src="/icon1.svg" height={10} width={10} />
         </div>
       </div>
     </div>
