c1f175b2c25ed932b8550698f27865e557ab8b94
code <code@peepulagri.com>
2024-07-13 14:38:53 +0530

    sw/add styles to view device page

--- src/components/Devices/SingleDeviceView.tsx
+++ src/components/Devices/SingleDeviceView.tsx
@@ -30,7 +30,7 @@ const SingleDeviceView = () => {
     }, []);
 
     return (
-        <div>
+        <div id="deviceViewPage">
             <Button
                 variant="outlined"
                 className="backBtn"
@@ -39,93 +39,140 @@ const SingleDeviceView = () => {
                 startIcon={<Image src="/users/back-icon.svg" alt="" width={13} height={13} />}  >
                 Back
             </Button>
-            <p>{deviceData?.device_name || '--'}</p>
-            <Grid container spacing={2} sx={{ padding: 2 }}>
+            <div className="deviceinfo">
+                <Typography className="deviceName">{deviceData?.device_name || '--'}</Typography>
+                <Typography className="deviceNum">Device Number: <span className="deviceName">{deviceData?.device_id || '--'}</span> </Typography>
+            </div>
+            <Grid container spacing={2} className="deviceInfoContainer">
+                <Grid item xs={12} md={6}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={12}>
+                            <Paper className="eachDetailsCard">
+                                <div className="cardHeader">
+                                    <Typography variant="h6">Voltage Measurements</Typography>
+                                </div>
+                                <div className="cardBody">
+
+                                    {Object?.keys(deviceData)?.length
+                                        ?
+                                        Object?.keys(deviceData?.voltage_measurements)?.map((item, index) => (
+                                            <div className="eachBodyInfo" key={index}>
+                                                <label >{item}</label>
+                                                <Typography > {deviceData?.voltage_measurements[item] || '--'}</Typography>
+                                            </div>
+                                        ))
+                                        : ""
+                                    }
+                                </div>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs={12} md={12} >
+                            <Paper className="eachDetailsCard">
+                                <div className="cardHeader">
+                                    <Typography variant="h6">Errors</Typography>
+                                </div>
+                                <div className="cardBody">
+
+                                    {Object?.keys(deviceData)?.length
+                                        ?
+                                        Object?.keys(deviceData?.errors).map((item, index) => (
+                                            <div className="eachBodyInfo" key={index}>
+                                                <label >{item}</label>
+
+                                                <Typography> {deviceData?.errors[item] === true ? "Error" : "" || '--'}</Typography>
+                                            </div>
+                                        ))
+                                        : ""
+                                    }
+                                </div>
+                            </Paper>
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Paper className="eachDetailsCard">
+                        <div className="cardHeader">
+                            <Typography variant="h6">Power Measurements</Typography>
+                        </div>
+                        <div className="cardBody">
+
+                            {Object?.keys(deviceData)?.length
+                                ?
+                                Object?.keys(deviceData?.power_measurements).map((item, index) => (
+                                    <div className="eachBodyInfo" key={index}>
+                                        <label >{item}</label>
+
+                                        <Typography>{deviceData?.power_measurements[item] || '--'}</Typography>
+                                    </div>
+                                ))
+                                : ""
+                            }
+                        </div>
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                    <Paper className="eachDetailsCard">
+                        <div className="cardHeader">
+                            <Typography variant="h6">Relay Status</Typography>
+                        </div>
+                        <div className="cardBody">
+
+                            {Object?.keys(deviceData)?.length
+                                ?
+                                Object?.keys(deviceData?.relay_status).map((item, index) => (
+                                    <div className="eachBodyInfo" key={index}>
+                                        <label >{item}</label>
+
+                                        <Typography> {deviceData?.relay_status[item] || '--'}</Typography>
+                                    </div>
+                                ))
+                                : ""
+                            }
+                        </div>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Paper className="eachDetailsCard">
+                        <div className="cardHeader">
+                            <Typography variant="h6">Bank Values</Typography>
+                        </div>
+                        <div className="cardBody">
+
+                            {Object?.keys(deviceData)?.length
+                                ?
+                                Object?.keys(deviceData?.bank_values).map((item, index) => (
+                                    <div className="eachBodyInfo" key={index}>
+                                        <label >{item}</label>
+
+                                        <Typography> {deviceData?.bank_values[item] || '--'}</Typography>
+                                    </div>
+                                ))
+                                : ""
+                            }
+                        </div>
+                    </Paper>
+                </Grid>
                 <Grid item xs={12}>
-                    <Typography variant="h6">Device Number: {deviceData?.device_id || '--'}</Typography>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper sx={{ padding: 2 }}>
-                        <Typography variant="subtitle1">Voltage Measurements</Typography>
-                        {deviceData?.length
-                            ?
-                            Object?.keys(deviceData?.voltage_measurements)?.map((item, index) => (
-                                <div key={index}>
-                                    <Typography>{item}: {deviceData?.voltage_measurements[item] || '--'}</Typography>
-                                </div>
-                            ))
-                            : ""
-                        }
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper sx={{ padding: 2 }}>
-                        <Typography variant="subtitle1">Power Measurements</Typography>
-                        {deviceData?.length
-                            ?
-                            Object?.keys(deviceData?.power_measurements).map((item, index) => (
-                                <div key={index}>
-                                    <Typography>{item}: {deviceData?.power_measurements[item] || '--'}</Typography>
-                                </div>
-                            ))
-                            : ""
-                        }
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper sx={{ padding: 2 }}>
-                        <Typography variant="subtitle1">Errors</Typography>
-                        {deviceData?.length
-                            ?
-                            Object?.keys(deviceData?.errors).map((item, index) => (
-                                <div key={index}>
-                                    <Typography>{item}: {deviceData?.errors[item] === true ? "Error" : "" || '--'}</Typography>
-                                </div>
-                            ))
-                            : ""
-                        }
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper sx={{ padding: 2 }}>
-                        <Typography variant="subtitle1">Relay Status</Typography>
-                        {deviceData?.length
-                            ?
-                            Object?.keys(deviceData?.relay_status).map((item, index) => (
-                                <div key={index}>
-                                    <Typography>{item}: {deviceData?.relay_status[item] || '--'}</Typography>
-                                </div>
-                            ))
-                            : ""
-                        }
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper sx={{ padding: 2 }}>
-                        <Typography variant="subtitle1">Bank Values</Typography>
-                        {deviceData?.length
-                            ?
-                            Object?.keys(deviceData?.bank_values).map((item, index) => (
-                                <div key={index}>
-                                    <Typography>{item}: {deviceData?.bank_values[item] || '--'}</Typography>
-                                </div>
-                            ))
-                            : ""
-                        }
-                    </Paper>
-                </Grid>
-                <Grid item xs={12}>
-                    <Paper sx={{ padding: 2 }}>
-                        <Typography variant="subtitle1">Total Harmonic Distortion (THD)</Typography>
-                        {deviceData?.length
-                            ?
-                            Object?.keys(deviceData?.total_harmonic_distortion).map((item, index) => (
-                                <div key={index}>
-                                    <Typography>{item}: {deviceData?.total_harmonic_distortion[item] || '--'}</Typography>
-                                </div>
-                            ))
-                            : ""
-                        }
+                    <Paper className="eachDetailsCard">
+                        <div className="cardHeader">
+                            <Typography variant="h6">Total Harmonic Distortion (THD)</Typography>
+                        </div>
+                        <div className="cardBody">
+
+                            {Object?.keys(deviceData)?.length
+                                ?
+                                Object?.keys(deviceData?.total_harmonic_distortion).map((item, index) => (
+                                    <div className="eachBodyInfo" key={index}>
+                                        <label >{item}</label>
+
+                                        <Typography> {deviceData?.total_harmonic_distortion[item] || '--'}</Typography>
+                                    </div>
+                                ))
+                                : ""
+                            }
+                        </div>
                     </Paper>
                 </Grid>
             </Grid>
