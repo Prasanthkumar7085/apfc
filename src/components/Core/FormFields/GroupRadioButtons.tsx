cce9e985a9ebf92898dd2dbce469af3202ac9f0b
code-peepulagri <code@peepulagri.com>
2024-07-17 12:51:56 +0530

    vn/Updated leve1 settings

--- src/components/Core/FormFields/GroupRadioButtons.tsx
+++ src/components/Core/FormFields/GroupRadioButtons.tsx
@@ -7,29 +7,26 @@ import {
 } from "@mui/material";
 
 const GroupRadioButtons = ({ setting, handleChange, formData }: any) => {
+
   return (
     <div className="radioGrp">
-      {formData?.[setting?.name] ? (
-        <FormControl>
-          <RadioGroup
-            aria-labelledby="demo-controlled-radio-buttons-group"
-            name={setting?.name}
-            value={formData?.[setting?.name]}
-            onChange={handleChange}
-          >
-            {setting.options?.map((option: any, index: any) => (
-              <FormControlLabel
-                value={option?.value}
-                control={<Radio />}
-                label={option?.label}
-                key={index}
-              />
-            ))}
-          </RadioGroup>
-        </FormControl>
-      ) : (
-        ""
-      )}
+      <FormControl>
+        <RadioGroup
+          aria-labelledby="demo-controlled-radio-buttons-group"
+          name={setting?.name}
+          value={formData?.[setting?.name] ? formData?.[setting?.name] : false}
+          onChange={handleChange}
+        >
+          {setting.options?.map((option: any, index: any) => (
+            <FormControlLabel
+              value={option?.value}
+              control={<Radio />}
+              label={option?.label}
+              key={index}
+            />
+          ))}
+        </RadioGroup>
+      </FormControl>
     </div>
   );
 };
